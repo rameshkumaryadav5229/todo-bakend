19b81c29952f926327d1f5e900a6d06936215907
Ramesh Kumar Yadav <153163693+rameshkumaryadav5229@users.noreply.github.com>
2025-08-16 21:36:13 +0530

    Delete Book.js

--- Book.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const bookSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  image: String,
-});
-
-module.exports = mongoose.model("Book", bookSchema);
